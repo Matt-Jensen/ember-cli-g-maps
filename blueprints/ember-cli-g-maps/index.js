4173cd156725620e2df80bbcc45de27c37b2d2a6
Matt-Jensen <matt@jwc.works>
2015-08-05 13:19:07 -0500

    Fixed ember-addon publishing and dependency registration, added documentation readme

---
+++ blueprints/ember-cli-g-maps/index.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // description: '',
+
+  // locals: function(options) {
+  //   // Return custom template variables here.
+  //   return {
+  //     foo: options.entity.options.foo
+  //   };
+  // }
+
+  normalizeEntityName: function() {},
+
+  afterInstall: function() {
+    return this.addBowerPackageToProject('gmaps');
+  }
+};
