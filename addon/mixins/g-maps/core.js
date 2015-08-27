682040f9f562281321b2f4ebad77fa13b12de563
Matt <mjensen@oaganalytics.com>
2015-08-27 18:48:48 -0500

    Abstracted out g-maps component into core mixin for unit testing.  Added g-maps/core unit tests.  Succumbed to OCD.

---
+++ addon/mixins/g-maps/core.js
@@ -0,0 +1,283 @@
+/* globals GMaps: true, google: true */
+import Ember from 'ember';
+
+const { later } = Ember.run;
+const { on, merge, uuid, computed, observer } = Ember;
+
+export default Ember.Mixin.create({
+  map: null,
+  name: null,
+  draggable: true,
+  isMapLoaded: false,
+  classNames: ['ember-cli-g-map'], 
+  gMap: Ember.inject.service(),
+
+  // Map Events
+  _gmapEvents: [
+    'idle',
+    'drag',
+    'click',
+    'resize',
+    'dragend',
+    'dblclick',
+    'mouseout',
+    'dragstart',
+    'mousemove',
+    'mouseover',
+    'rightclick',
+    'tilesloaded',
+    'tilt_changed',
+    'zoom_changed',
+    'bounds_changed',
+    'center_changed',
+    'heading_changed',
+    'maptypeid_changed',
+    'projection_changed'
+  ],
+
+  _requiredProperties: {
+    lat: 0,
+    lng: 0,
+    zoom: 0,
+    mapType: 'ROADMAP',
+    mapTypeControl: false,
+  },
+
+  _initGMap: on('didInsertElement', function() {
+    const events      = this.get('_gmapEvents');
+    const configProps = ['lat', 'lng', 'zoom'];
+    let config        = this.getProperties.apply(this, configProps);
+    config            = merge(this._requiredProperties, config);
+    config.div        = `#${this.element.id}`;
+
+    const map = new GMaps( config );
+    this.set('map', map);
+
+    // Set GMap events
+    for(let i = 0, l = events.length; i < l; i++ ) {
+
+      // If map event defined on component
+      if( !this.get(events[i]) ) { continue; }
+
+      // Add GMaps event listener on google map instance
+      GMaps.on(events[i], map.map, (e) => this.send(events[i], e));
+    }
+
+    if( !this.get('name') ) {
+      this.set('name', `ember-cli-g-map-${uuid()}`);
+    }
+
+    // Create map service instance and register load event
+    const mapService = this.get('gMap').maps.add(this.get('name'), map.map);
+    mapService.onLoad.then(() => {
+      this.set('isMapLoaded', true);
+      this.trigger('ember-cli-g-map-loaded');
+    });
+  }),
+
+
+  _destroyGMap: on('willDestroyElement', function destroyGMap() {
+    const events = this.get('_gmapEvents');
+    const map    = this.get('map');
+
+    // Remove GMap events
+    for(let i = 0, l = events.length; i < l; i++ ) {
+
+      // If map event defined on component
+      if( !this.get(events[i]) ) { continue; }
+      GMaps.off(events[i], map.map);
+    }
+
+    // Remove GMap from gMap service
+    this.get('gMap').maps.remove(this.get('name'));
+
+    // Run after Mixin willDestroyElement
+    later(() => this.get('map').destroy());
+  }),
+
+
+  /////////////////////////////
+  // Parent -> GMap Bindings
+  /////////////////////////////
+
+  _syncCenter: observer('isMapLoaded', 'lat', 'lng', function() {
+    if(!this.get('isMapLoaded')) { return; }
+    const { map, lat, lng } = this.getProperties('map', 'lng', 'lat');
+    const { A, F } = map.getCenter();
+    const areCoordsEqual = this._areCoordsEqual;
+
+    // If map is out of sync with app state
+    if(!areCoordsEqual(A, lat) || !areCoordsEqual(F, lng)) {
+      map.setCenter(lat, lng);
+    }
+  }),
+
+  _syncZoom: observer('isMapLoaded', 'zoom', function() {
+    if(!this.get('isMapLoaded')) { return; }
+    const { map, zoom } = this.getProperties('map', 'zoom');
+    map.setZoom(zoom);
+  }),
+
+  _syncDraggable: observer('isMapLoaded', 'draggable', function() {
+    if(!this.get('isMapLoaded')) { return; }
+    const map = this.get('map').map;
+    map.setOptions({ draggable: this.get('draggable') });
+  }),
+
+  _syncMapType: observer('isMapLoaded', 'mapType', function() {
+    if(!this.get('isMapLoaded')) { return; }
+    const map     = this.get('map').map;
+    const mapType = this.get('mapType')+'';
+
+    if(mapType === 'undefined') { return; }
+
+    if(mapType.toLowerCase() !== map.getMapTypeId()) {
+      map.setMapTypeId( google.maps.MapTypeId[mapType.toUpperCase()] );
+    }
+  }),
+
+
+  /////////////////////////////
+  // GMap -> Parent Bindings
+  ////////////////////////////
+
+  _addGMapPersisters: on('ember-cli-g-map-loaded', function() {
+    const map = this.get('map');
+
+    GMaps.on('center_changed', map.map, () => { 
+      Ember.run.debounce(this, this._onCenterChanged, 100);
+    });
+
+    GMaps.on('zoom_changed', map.map, () => {
+      later(() => this._onZoomChanged());
+    });
+  }),
+
+  _onCenterChanged: function() {
+    const map = this.get('map');
+    const areCoordsEqual = this._areCoordsEqual;
+    const { lat, lng } = this.getProperties('lat', 'lng');
+    const { A, F } = map.getCenter();
+
+    // If app state is out of sync with GMap
+    if(!areCoordsEqual(A, lat) || !areCoordsEqual(F, lng)) {
+      this.setProperties({
+        lat: A,
+        lng: F
+      });
+    }
+  },
+
+  _onZoomChanged: function() {
+    const map = this.get('map');
+    const zoom = this.get('zoom');
+
+    // Zooming changes lat,lng state
+    const { A, F } = map.getCenter();
+
+    if(zoom !== map.map.zoom) {
+      this.setProperties({
+        zoom: map.map.zoom,
+        lat: A,
+        lng: F
+      });
+    }
+  },
+
+
+  /////////////////////////////////////////////////////////////
+  // Map state info, generally required info to make requests
+  /////////////////////////////////////////////////////////////
+
+  defaultGMapState: computed('lat', 'lng', 'zoom', function() {
+    const map    = this.get('map');
+    const bounds = map.map.getBounds();
+
+    return {
+      bounds: [
+        { lat: bounds.Da.j, lng: bounds.va.j }, // top left
+        { lat: bounds.Da.j, lng: bounds.va.A }, // top right
+        { lat: bounds.Da.A, lng: bounds.va.A }, // bottom left
+        { lat: bounds.Da.A, lng: bounds.va.j }  // bottom right
+      ],
+
+      mapIdle: new Promise((resolve) => {
+        google.maps.event.addListenerOnce(map.map, 'idle', resolve);
+      }),
+
+      mapTilesLoaded: new Promise((resolve) => {
+        google.maps.event.addListenerOnce(map.map, 'tilesloaded', resolve);
+      })
+    };
+  }),
+
+
+  // Supported g-map Actions
+
+  actions: {
+    idle: function() {
+      this.sendAction('idle', merge(this.get('defaultGMapState'), ...arguments));
+    },
+    drag: function() {
+      this.sendAction('drag', merge(this.get('defaultGMapState'), ...arguments));
+    },
+    click: function() {
+      this.sendAction('click', merge(this.get('defaultGMapState'), ...arguments));
+    },
+    resize: function() {
+      this.sendAction('resize', merge(this.get('defaultGMapState'), ...arguments));
+    },
+    dragend: function() {
+      this.sendAction('dragend', merge(this.get('defaultGMapState'), ...arguments));
+    },
+    dblclick: function() {
+      this.sendAction('dblclick', merge(this.get('defaultGMapState'), ...arguments));
+    },
+    mouseout: function() {
+      this.sendAction('mouseout', merge(this.get('defaultGMapState'), ...arguments));
+    },
+    dragstart: function() {
+      this.sendAction('dragstart', merge(this.get('defaultGMapState'), ...arguments));
+    },
+    mousemove: function() {
+      this.sendAction('mousemove', merge(this.get('defaultGMapState'), ...arguments));
+    },
+    mouseover: function() {
+      this.sendAction('mouseover', merge(this.get('defaultGMapState'), ...arguments));
+    },
+    rightclick: function() {
+      this.sendAction('rightclick', merge(this.get('defaultGMapState'), ...arguments));
+    },
+    tilesloaded: function() {
+      this.sendAction('tilesloaded', merge(this.get('defaultGMapState'), ...arguments));
+    },
+    tilt_changed: function() {
+      this.sendAction('tilt_changed', merge(this.get('defaultGMapState'), ...arguments));
+    },
+    zoom_changed: function() {
+      this.sendAction('zoom_changed', merge(this.get('defaultGMapState'), ...arguments));
+    },
+    bounds_changed: function() {
+      this.sendAction('bounds_changed', merge(this.get('defaultGMapState'), ...arguments));
+    },
+    center_changed: function() {
+      this.sendAction('center_changed', merge(this.get('defaultGMapState'), ...arguments));
+    },
+    heading_changed: function() {
+      this.sendAction('heading_changed', merge(this.get('defaultGMapState'), ...arguments));
+    },
+    maptypeid_changed: function() {
+      this.sendAction('maptypeid_changed', merge(this.get('defaultGMapState'), ...arguments));
+    },
+    projection_changed: function() {
+      this.sendAction('projection_changed', merge(this.get('defaultGMapState'), ...arguments));
+    }
+  },
+
+
+  /////////////
+  // Helpers
+  ////////////
+  
+  _areCoordsEqual: (a, b) => a.toFixed(12) === b.toFixed(12)
+});
---
+++ tests/unit/mixins/g-maps/core-test.js
@@ -0,0 +1,259 @@
+import Ember            from 'ember';
+import GMapsCoreMixin   from 'ember-cli-g-maps/mixins/g-maps/core';
+import { module, test } from 'qunit';
+import sinon            from 'sinon';
+
+let subject, mapDiv;
+const mapID = 'test-map';
+const emberTestingDiv = document.getElementById('ember-testing');
+
+module('Unit | Mixin | g maps/core', {
+  beforeEach: function() {
+    mapDiv = document.createElement('div');
+    mapDiv.id = mapID;
+    emberTestingDiv.appendChild(mapDiv);
+    const GMapsCoreObject = Ember.Object.extend(GMapsCoreMixin, Ember.Evented, {
+      element: mapDiv,
+
+      // Mock gMap service
+      gMap: {
+        maps: { 
+          add: function() {
+            return {
+              onLoad: new Promise((resolve) => {})
+            };
+          },
+          remove: function() {}
+        }
+      },
+
+      send: function() {}
+    });
+    subject = GMapsCoreObject.create();
+  },
+  afterEach: function() {
+    const gm = subject.get('map');
+    emberTestingDiv.removeChild(mapDiv);
+
+    // Clean up any instantiated GMaps
+    Ember.run.later(() => {
+      if(gm && gm.map) { gm.destroy(); }
+    }, 10); // run after `_destroyGMap` if needed
+  }
+});
+
+
+///////////////
+// Init Core
+//////////////
+
+test('`_initGMap` should invoke on `didInsertElement` event', function(assert) {
+  subject._initGMap = sinon.spy();
+  subject.trigger('didInsertElement');
+  assert.ok(subject._initGMap.called);
+});
+
+test('`_initGMap` should create a GMap instance with main config properties', function(assert) {
+  const config = { lat: 1, lng: 1, zoom: 10 };
+
+  subject.setProperties(config);
+  subject._initGMap();
+
+  const zoom = subject.get('map').map.zoom;
+  const { A, F } = subject.get('map').map.getCenter();
+
+  assert.equal(A, config.lat);
+  assert.equal(F, config.lng);
+  assert.equal(zoom, config.zoom);
+});
+
+test('`_initGMap` should set all configured gmap events', function(assert) {
+  const noop            = function() {};
+  const addedEvents     = [];
+  const originalGMapsOn = GMaps.on;
+  const supportedEvents = subject.get('_gmapEvents');
+
+  GMaps.on = function(event) {
+    addedEvents.push(event);
+  };
+
+  // Add all supported events
+  supportedEvents.forEach((e) => subject.set(e, noop));
+
+  subject._initGMap();
+  
+  assert.deepEqual(addedEvents, supportedEvents);
+
+  GMaps.on = originalGMapsOn;
+});
+
+test('`_initGMap` should set a given name if provided', function(assert) {
+  subject.set('name', 'my-cool-map');
+  subject._initGMap();
+
+  assert.equal(subject.get('name'), 'my-cool-map');
+});
+
+test('`_initGMap` should automatically set a `name` if none provided', function(assert) {
+  subject._initGMap();
+  assert.ok(subject.get('name').length);
+});
+
+test('`_initGMap` should add a map to the `gMap` service with same `name`', function(assert) {
+  assert.expect(1);
+  subject.set('name', 'Marmaduke');
+
+  subject.gMap.maps.add = function(name) {
+    assert.equal(name, 'Marmaduke');
+    return { onLoad: new Promise(() => {}) };
+  };
+
+  subject._initGMap();
+});
+
+
+/////////////////
+// Destroy Core
+/////////////////
+
+test('`_destroyGMap` should invoke on `willDestroyElement` event', function(assert) {
+  subject._destroyGMap = sinon.spy();
+  subject.trigger('willDestroyElement');
+  assert.ok(subject._destroyGMap.called);
+});
+
+test('`_destroyGMap` should remove all bound GMap events', function(assert) {
+  const noop             = function() {};
+  const removedEvents    = [];
+  const originalGMapsOff = GMaps.off;
+  const supportedEvents  = subject.get('_gmapEvents');
+
+  GMaps.off = function(event) {
+    removedEvents.push(event);
+  };
+
+  // Add all supported events
+  supportedEvents.forEach((e) => subject.set(e, noop));
+  subject._initGMap();
+  subject._destroyGMap();
+
+  assert.deepEqual(removedEvents, supportedEvents);
+
+  GMaps.off = originalGMapsOff;
+});
+
+test('`_destroyGMap` should remove gMap instance added to gMap service', function(assert) {
+  assert.expect(1);
+
+  subject.set('name', 'may-test-map')
+  subject.gMap.maps.remove = function(name) {
+    assert.equal(name, 'may-test-map');
+  };
+  subject._initGMap();
+  subject._destroyGMap();
+});
+
+// test('`_destroyGMap` should call `destroy` on the GMap instance', function(assert) {});
+
+
+//////////////////
+// Sync Lat Lng
+/////////////////
+
+// test('`_syncCenter` should not sync `lat` & `lng` if `isMapLoaded` is false', function(assert) {});
+// test('`_syncCenter` should not sync if `map.{lat,lng}` equals `subject.{lat,lng}`', function(assert) {});
+// test('`_syncCenter` should sync map instance if out of sync with subject', function(assert) {});
+
+
+///////////////
+// Sync Zoom
+//////////////
+
+// test('`_syncZoom` should not sync `zoom` if `isMapLoaded` is false', function(assert) {});
+// test('`_syncZoom` should sync zoom if `isMapLoaded` is true', function(assert) {});
+
+
+////////////////////
+// Sync Draggable
+///////////////////
+
+// test('`_syncDraggable` should not sync `draggable` if `isMapLoaded` is false', function(assert) {});
+// test('`_syncDraggable` should sync `draggable` if `isMapLoaded` is true', function(assert) {});
+
+
+///////////////////
+// Sync Map Type
+//////////////////
+
+// test('`_syncMapType` should not sync `mapType` if `isMapLoaded` is false', function(assert) {});
+// test('`_syncMapType` should not sync `mapType` if `mapType` is undefined', function(assert) {});
+// test('`_syncMapType` should sync set `mapType` if `isMapLoaded` is true', function(assert) {});
+
+
+/////////////////////////////
+// GMap Persistence Events
+////////////////////////////
+
+// test('`_addGMapPersisters` should add GMap events on `ember-cli-g-map-loaded`', function(assert) {
+  // `center_changed`
+  // `zoom_changed`
+// });
+// test('`_addGMapPersisters` should call `_onCenterChanged` on GMap event: `center_changed` fire', function(assert) {});
+// test('`_addGMapPersisters` should call `_onZoomChanged` on GMap event: `zoom_changed` fire', function(assert) {});
+
+
+///////////////////////
+// On Center Changed
+//////////////////////
+
+// test('`_onCenterChanged` should not sync if same as subject `lat`,`lng`', function(assert) {});
+// test('`_onCenterChanged` should sync new `lat` & `lng` to subject', function(assert) {});
+
+
+/////////////////////
+// On Zoom Changed
+////////////////////
+
+// test('`_onZoomChanged` should not sync if same as subject `zoom`', function(assert) {});
+// test('`_onZoomChanged` should sync new `zoom`, `lat`, and `zoom` to subject', function(assert) {});
+
+
+////////////////////////
+// Default GMap State
+/////////////////////// 
+
+// test('`defaultGMapState` should return the current map bounds', function(assert) {});
+// test('`defaultGMapState` should return a `mapIdle` Promise', function(assert) {});
+// test('`defaultGMapState` should return a `mapTilesLoaded` Promise', function(assert) {});
+
+
+/////////////
+// Actions
+////////////
+
+// test('it should send `idle` on GMap event: `idle` fire', function(assert) {});
+// test('it should send `drag` on GMap event: `drag` fire', function(assert) {});
+// test('it should send `click` on GMap event: `click` fire', function(assert) {});
+// test('it should send `resize` on GMap event: `resize` fire', function(assert) {});
+// test('it should send `dragend` on GMap event: `dragend` fire', function(assert) {});
+// test('it should send `dblclick` on GMap event: `dblclick` fire', function(assert) {});
+// test('it should send `mouseout` on GMap event: `mouseout` fire', function(assert) {});
+// test('it should send `dragstart` on GMap event: `dragstart` fire', function(assert) {});
+// test('it should send `mousemove` on GMap event: `mousemove` fire', function(assert) {});
+// test('it should send `mouseover` on GMap event: `mouseover` fire', function(assert) {});
+// test('it should send `rightclick` on GMap event: `rightclick` fire', function(assert) {});
+// test('it should send `tilesloaded` on GMap event: `tilesloaded` fire', function(assert) {});
+// test('it should send `tilt_changed` on GMap event: `tilt_changed` fire', function(assert) {});
+// test('it should send `zoom_changed` on GMap event: `zoom_changed` fire', function(assert) {});
+// test('it should send `bounds_changed` on GMap event: `bounds_changed` fire', function(assert) {});
+// test('it should send `center_changed` on GMap event: `center_changed` fire', function(assert) {});
+// test('it should send `heading_changed` on GMap event: `heading_changed` fire', function(assert) {});
+// test('it should send `maptypeid_changed` on GMap event: `maptypeid_changed` fire', function(assert) {});
+// test('it should send `projection_changed` on GMap event: `projection_changed` fire', function(assert) {});
+
+
+/////////////
+// Helpers
+////////////
+
+// test('`_areCoordsEqual` should correctly compare 2 numbers within 12 digits', function(assert) {});
