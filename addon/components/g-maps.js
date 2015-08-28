1e01c11aadfee514b1298393cd15ea8b7f8f8d3e
Matt-Jensen <matt@jwc.works>
2015-08-27 23:40:39 -0500

    added unit test to g-maps/core mixin.

--- addon/components/g-maps.js
+++ addon/components/g-maps.js
@@ -1,16 +1,16 @@
 import Ember          from 'ember';
 
+import GMapCore       from 'ember-cli-g-maps/mixins/g-maps/core';
+
 // Map Childs //
+import GMapCircles    from 'ember-cli-g-maps/mixins/g-maps/circles';
 import GMapMarkers    from 'ember-cli-g-maps/mixins/g-maps/markers';
 import GMapPolygons   from 'ember-cli-g-maps/mixins/g-maps/polygons';
-import GMapCircles    from 'ember-cli-g-maps/mixins/g-maps/circles';
 import GMapPolylines  from 'ember-cli-g-maps/mixins/g-maps/polylines';
 import GMapRectangles from 'ember-cli-g-maps/mixins/g-maps/rectangles';
 
 // Extensions //
 import GMapSelections from 'ember-cli-g-maps/mixins/g-maps/selections';
-
-import GMapCore       from 'ember-cli-g-maps/mixins/g-maps/core';
 
 export default Ember.Component.extend(
   Ember.Evented,
