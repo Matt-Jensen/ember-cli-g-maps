93a583f4b7f6fee4da8fb6037c75509d91bb73ed
Matt-Jensen <matt@jwc.works>
2015-08-29 19:39:02 -0500

    layout of heatmap mixin

---
+++ tests/unit/mixins/g-maps/heatmap-test.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+import GMapsHeatmapMixin from '../../../mixins/g-maps/heatmap';
+import { module, test } from 'qunit';
+
+module('Unit | Mixin | g maps/heatmap');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var GMapsHeatmapObject = Ember.Object.extend(GMapsHeatmapMixin);
+  var subject = GMapsHeatmapObject.create();
+  assert.ok(subject);
+});
