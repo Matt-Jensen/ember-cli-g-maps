4ede345bef1b2294118f62b34b40bf3a2b15fd23
Matt-Jensen <matt@jwc.works>
2015-09-08 23:20:46 -0500

    closes #15. Abstracts parent route template into component.

--- tests/dummy/app/pods/setup/route.js
+++ tests/dummy/app/pods/setup/route.js
@@ -1,3 +1,34 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend();
+export default Ember.Route.extend({
+  setupController: function(controller) {
+    controller.set('setupRoutes', [
+      {
+        label: false,
+        links: [
+          {
+            href: 'setup.index',
+            text: 'Installation'
+          }
+        ]
+      },
+      {
+        label: 'Configuration',
+        links: [
+          {
+            href: 'setup.apiKey',
+            text: 'API Key'
+          },
+          {
+            href: 'setup.libraries',
+            text: 'Adding Libraries'
+          },
+          {
+            href: 'setup.misc',
+            text: 'Misc Options'
+          }
+        ]
+      }
+    ]);
+  }
+});
