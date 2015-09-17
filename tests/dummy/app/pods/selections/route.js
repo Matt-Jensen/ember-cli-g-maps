5fa01eee586a244bdf1af3fec619f6adc5a18464
Matt-Jensen <matt@jwc.works>
2015-09-17 18:40:30 -0500

    closes #23 adds services coming soon documentation updates readme with new links removes superfluous readme examples

--- tests/dummy/app/pods/selections/route.js
+++ tests/dummy/app/pods/selections/route.js
@@ -1,3 +1,50 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend();
+export default Ember.Route.extend({
+  setupController: function(controller) {
+    controller.set('setupRoutes', [
+      {
+        label: false,
+        links: [
+          {
+            href: 'selections.index',
+            text: 'Setup'
+          },
+          {
+            href: 'selections.properties',
+            text: 'Properties'
+          },
+          {
+            href: 'selections.events',
+            text: 'Events'
+          }
+        ]
+      },
+      {
+        label: 'Selectors',
+        links: [
+          {
+            href: 'selections.marker',
+            text: 'Marker'
+          },
+          {
+            href: 'selections.rectangle',
+            text: 'Rectangle'
+          },
+          {
+            href: 'selections.circle',
+            text: 'Circle'
+          },
+          {
+            href: 'selections.polygon',
+            text: 'Polygon'
+          },
+          {
+            href: 'selections.polyline',
+            text: 'Polyline'
+          }
+        ]
+      }
+    ]);
+  }
+});
