8b2466f7e8f80086df2e99f5662fc8624afe9ce9
Steven Wu <wusteven@gmail.com>
2016-01-29 11:49:49 -0800

    Lookup service on app container

--- test-support/helpers/ember-cli-g-maps/stub-autocomplete.js
+++ test-support/helpers/ember-cli-g-maps/stub-autocomplete.js
@@ -1,15 +1,5 @@
 import Ember from 'ember';
 
 export default function(app, test) {
-  app.instanceInitializer({
-    name: 'stubGMapAutocomplete',
-    initialize(application) {
-      let emberVersion = parseFloat(Ember.VERSION);
-      if (emberVersion >= 2.1) {
-        test.gMapService = application.lookup('service:g-map');
-      } else {
-        test.gMapService = application.registry.lookup('service:g-map');
-      }
-    }
-  });
+  test.gMapService = app.__container__.lookup('service:g-map');
 };
