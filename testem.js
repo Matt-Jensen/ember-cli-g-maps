d2980a86e5dc8fffb38d559798b802e716562876
Matt-Jensen <matt@jwc.works>
2016-04-19 12:28:08 -0400

    migrate to ember-cli v2.5.0

--- testem.js
+++ testem.js
@@ -2,6 +2,7 @@
 module.exports = {
   "framework": "qunit",
   "test_page": "tests/index.html?hidepassed",
+  "phantomjs_debug_port": 9000,
   "disable_watching": true,
   "launch_in_ci": [
     "PhantomJS"
