fcf6d51a124ddcbe5bfa41c66fd54a0995055aca
Matt-Jensen <matt@jwc.works>
2016-12-10 14:17:41 -0600

    add 	`waitForGoogleMap` acceptance test helper

--- test-support/helpers/ember-cli-g-maps/register-async-helpers.js
+++ test-support/helpers/ember-cli-g-maps/register-async-helpers.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import selectAutocompletePlace from './select-autocomplete-place-helper';
+import waitForGoogleMap from './wait-for-google-map-helper';
 
 export default function() {
   Ember.Test.registerAsyncHelper('selectPlace', () => {
@@ -7,4 +8,5 @@ export default function() {
 Usage details here: http://http://matt-jensen.github.io/ember-cli-g-maps/#/place-autocomplete/index`)
   });
   Ember.Test.registerAsyncHelper('selectAutocompletePlace', selectAutocompletePlace);
+  Ember.Test.registerAsyncHelper('waitForGoogleMap', waitForGoogleMap);
 }
