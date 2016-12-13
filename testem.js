5faffc538506c0f715ffcab0ea29ee93477f79df
Matt-Jensen <matt@jwc.works>
2016-12-13 15:36:49 -0600

    add phantomjs debug port to tested config

--- testem.js
+++ testem.js
@@ -3,6 +3,7 @@ module.exports = {
   "framework": "qunit",
   "test_page": "tests/index.html?hidepassed",
   "disable_watching": true,
+  "phantomjs_debug_port": 9000,
   "launch_in_ci": [
     "PhantomJS"
   ],
