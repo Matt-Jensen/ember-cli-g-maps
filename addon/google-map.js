06f58f90a6fb50286e62b4bd9b1e1f68dfca1d43
Matt-Jensen <matt@jwc.works>
2016-12-14 21:31:36 -0600

    #66 refactor google map proxy and add properties

--- addon/google-map.js
+++ addon/google-map.js
@@ -10,167 +10,249 @@ const MAP_DEFAULTS = {
   tilt: 0
 };
 
+const getStaticMapOption = function(key) {
+  return this.content[key];
+};
+
+const setStaticMapBooleanOption = function(key, value) {
+  assert(`${key} was set without boolean`, typeof value === 'boolean');
+  this.content.setOptions({[key]:value});
+  return this.get(key);
+};
+
+const setStaticMapStringOption = function(key, value) {
+  assert(`${key} was set without string`, typeof value === 'string');
+  this.content.setOptions({[key]:value});
+  return this.get(key);
+};
+
+export const GoogleMapProxy = Ember.ObjectProxy.extend({
+  /**
+   * @type {Object}
+   * Update the center of the Google Map instance via LatLngLiterals
+   */
+  center: computed({
+    get() {
+      const center = this.content.getCenter();
+      return { lat: center.lat(), lng: center.lng() };
+    },
+
+    set(key, {lat, lng}) {
+      assert('center was set without a lat number', typeof lat === 'number');
+      assert('center was set without a lng number', typeof lng === 'number');
+      this.content.setCenter({lat, lng});
+      return this.get('center');
+    }
+  }),
+
+  /**
+   * @type {Number}
+   * Map minimum zoom level
+   */
+  minZoom: computed({
+    get() {
+      return this.content.minZoom;
+    },
+
+    set(key, minZoom) {
+      assert('minZoom was set without number', typeof minZoom === 'number');
+      assert('minZoom was set above maxZoom', minZoom < this.get('maxZoom'));
+      return this.content.minZoom = minZoom;
+    }
+  }),
+
+  /**
+   * @type {Number}
+   * Map maximum zoom level
+   */
+  maxZoom: computed({
+    get() {
+      return this.content.maxZoom;
+    },
+
+    set(key, maxZoom) {
+      assert('maxZoom was set without number', typeof maxZoom);
+      assert('maxZoom was set below minZoom', maxZoom > this.get('minZoom'));
+      return this.content.maxZoom = maxZoom;
+    }
+  }),
+
+  /**
+   * @type {Number}
+   * Map zoom level
+   */
+  zoom: computed({
+    get() {
+      return this.content.getZoom();
+    },
+
+    set(key, zoom) {
+      const min = this.get('minZoom');
+      const max = this.get('maxZoom');
+
+      assert('zoom was set without a number', typeof zoom === 'number');
+      assert('zoom was set above maxZoom', zoom <= max);
+      assert('zoom was set below minZoom', zoom >= min);
+
+      this.content.setZoom(zoom);
+      return this.get('zoom');
+    }
+  }),
+
+  /**
+   * @type {String}
+   * Type of map rendered
+   */
+  mapTypeId: computed({
+    get() {
+      return this.content.getMapTypeId().toUpperCase();
+    },
+
+    set(key, mapTypeId) {
+      const mapTypes = Object.keys(google.maps.MapTypeId);
+
+      assert('mapTypeId was set without a string', typeof mapTypeId === 'string');
+      assert('mapTypeId is not a valid map type', mapTypes.indexOf(mapTypeId.toUpperCase()) > -1);
+
+      this.content.setMapTypeId(google.maps.MapTypeId[mapTypeId.toUpperCase()]);
+      return this.get('mapTypeId');
+    }
+  }),
+
+  /**
+   * @type {Boolean}
+   * Point of interest icon clickablity
+   */
+  clickableIcons: computed({
+    get() {
+      return this.content.getClickableIcons();
+    },
+
+    set(key, clickableIcons) {
+      assert('clickableIcons was set without a boolean', typeof clickableIcons === 'boolean');
+      this.content.setClickableIcons(clickableIcons);
+      return this.get('clickableIcons');
+    }
+  }),
+
+  /**
+   * @type {Number}
+   * view perspective if availble for map type and zoom
+   */
+  tilt: computed({
+    get() {
+      return this.content.getTilt();
+    },
+
+    set(key, tilt) {
+      assert('tilt was set without a number', typeof tilt === 'number');
+      assert('tilt is not `0` or `45`', tilt === 0 || tilt === 45);
+      this.content.setTilt(tilt);
+      return this.get('tilt');
+    }
+  }),
+
+  /**
+   * @type {Number}
+   * Heading for aerial imagery
+   */
+  heading: computed({
+    get() {
+      return this.content.getHeading();
+    },
+
+    set(key, heading) {
+      assert('heading was set without a number', typeof heading === 'number');
+      this.content.setHeading(heading);
+      return this.get('heading');
+    }
+  }),
+
+  /**
+   * @type {Boolean}
+   * Enables/disables all default UI
+   */
+  disableDefaultUI: computed({
+    get: getStaticMapOption,
+    set: setStaticMapBooleanOption
+  }),
+
+  /**
+   * @type {Boolean}
+   * Enables/disables zoom and center on double click
+   */
+  disableDoubleClickZoom: computed({
+    get: getStaticMapOption,
+    set: setStaticMapBooleanOption
+  }),
+
+  /**
+   * @type {Boolean}
+   * If false, prevents the map from being dragged
+   */
+  draggable: computed({
+    get: getStaticMapOption,
+    set: setStaticMapBooleanOption
+  }),
+
+  /**
+   * @type {String}
+   * The name or url of the cursor to display when mousing over a draggable map
+   */
+  draggableCursor: computed({
+    get: getStaticMapOption,
+    set: setStaticMapStringOption
+  }),
+
+  /**
+   * @type {Boolean}
+   * The enabled/disabled state of the Fullscreen control
+   */
+  fullscreenControl: computed({
+    get: getStaticMapOption,
+    set: setStaticMapBooleanOption
+  })
+
+  // gestureHandling String
+  // keyboardShortcuts Boolean
+  // mapTypeControl Boolean
+  // noClear Boolean
+  // panControl Boolean
+  // rotateControl Boolean
+  // scaleControl Boolean
+  // scrollwheel Boolean
+  // signInControl Boolean
+  // streetViewControl Boolean
+  // zoomControl Boolean
+  // styles [google.maps.MapTypeStyle]
+  // fullscreenControlOptions google.maps.FullscreenControlOptions
+  // mapTypeControlOptions google.maps.MapTypeControlOptions
+  // panControlOptions google.maps.PanControlOptions
+  // rotateControlOptions google.maps.RotateControlOptions
+  // scaleControlOptions google.maps.ScaleControlOptions
+  // streetViewControlOptions google.maps.StreetViewControlOptions
+  // zoomControlOption google.maps.ZoomControlOptions
+});
+
+/**
+ * @param  {HTMLElement} element  Google Map container element
+ * @param  {Object}      options  Map instance defaults
+ * @return {ObjectProxy}          Ember.ObjectProxy instance
+ * Generate a new Google Map proxy instance with given element and defaults
+ */
 export default function googleMap(element, options = {}) {
-  const settings = assign({}, MAP_DEFAULTS);
-  const map = new google.maps.Map(element, assign(settings, options));
+  assert('element must an HTMLElement', element instanceof HTMLElement);
 
-  const proxy = Ember.ObjectProxy.extend({
-    /**
-     * @type {Object}
-     * Update the center of the Google Map instance via LatLngLiterals
-     */
-    center: computed({
-      get() {
-        const center = this.content.getCenter();
-        return { lat: center.lat(), lng: center.lng() };
-      },
+  let settings = assign({}, MAP_DEFAULTS);
+  assign(settings, options);
 
-      set(key, {lat, lng}) {
-        assert('center was set without a lat number', typeof lat === 'number');
-        assert('center was set without a lng number', typeof lng === 'number');
-        this.content.setCenter({lat, lng});
-        return this.get('center');
-      }
-    }),
-
-    /**
-     * @type {Number}
-     * Minimum map zoom level
-     */
-    minZoom: computed({
-      get() {
-        return this.content.minZoom;
-      },
-
-      set(key, minZoom) {
-        assert('minZoom was set without number', typeof minZoom);
-        assert('minZoom was set above maxZoom', minZoom < this.get('maxZoom'));
-        return this.content.minZoom = minZoom;
-      }
-    }),
-
-    /**
-     * @type {Number}
-     * Maximum map zoom level
-     */
-    maxZoom: computed({
-      get() {
-        return this.content.maxZoom;
-      },
-
-      set(key, maxZoom) {
-        assert('maxZoom was set without number', typeof maxZoom);
-        assert('maxZoom was set below minZoom', maxZoom > this.get('minZoom'));
-        return this.content.maxZoom = maxZoom;
-      }
-    }),
-
-    /**
-     * @type {Number}
-     * Google map instance zoom level
-     */
-    zoom: computed({
-      get() {
-        return this.content.getZoom();
-      },
-
-      set(key, zoom) {
-        const min = this.get('minZoom');
-        const max = this.get('maxZoom');
-
-        assert('zoom was set without a number', typeof zoom === 'number');
-        assert('zoom was set above maxZoom', zoom <= max);
-        assert('zoom was set below minZoom', zoom >= min);
-
-        this.content.setZoom(zoom);
-        return this.get('zoom');
-      }
-    }),
-
-    /**
-     * @type {String}
-     * Google Map type
-     */
-    mapTypeId: computed({
-      get() {
-        return this.content.getMapTypeId().toUpperCase();
-      },
-
-      set(key, mapTypeId) {
-        const mapTypes = Object.keys(google.maps.MapTypeId);
-
-        assert('mapTypeId was set without a string', typeof mapTypeId === 'string');
-        assert('mapTypeId is not a valid map type', mapTypes.indexOf(mapTypeId.toUpperCase()) > -1);
-
-        this.content.setMapTypeId(google.maps.MapTypeId[mapTypeId.toUpperCase()]);
-        return this.get('mapTypeId');
-      }
-    }),
-
-    /**
-     * @type {Boolean}
-     * Google Map POI icon clickablity
-     */
-    clickableIcons: computed({
-      get() {
-        return this.content.getClickableIcons();
-      },
-
-      set(key, clickableIcons) {
-        assert('clickableIcons was set without a boolean', typeof clickableIcons === 'boolean');
-        this.content.setClickableIcons(clickableIcons);
-        return this.get('clickableIcons');
-      }
-    }),
-
-    /**
-     * @type {Number}
-     * Google Map tilt view perspective
-     */
-    tilt: computed({
-      get() {
-        return this.content.getTilt();
-      },
-
-      set(key, tilt) {
-        assert('tilt was set without a number', typeof tilt === 'number');
-        assert('tilt is not `0` or `45`', tilt === 0 || tilt === 45);
-        this.content.setTilt(tilt);
-        return this.get('tilt');
-      }
-    })
-
-    // heading number
-    // - getHeading()
-    // - setHeading()
-
-    // disableDefaultUI Boolean
-    // disableDoubleClickZoom Boolean
-    // draggable Boolean
-    // draggableCursor String
-    // draggingCursor String
-    // fullscreenControl Boolean
-    // fullscreenControlOptions google.maps.FullscreenControlOptions
-    // gestureHandling String
-    // keyboardShortcuts Boolean
-    // mapTypeControl Boolean
-    // mapTypeControlOptions google.maps.MapTypeControlOptions
-    // noClear Boolean
-    // panControl Boolean
-    // panControlOptions google.maps.PanControlOptions
-    // rotateControl Boolean
-    // rotateControlOptions google.maps.RotateControlOptions
-    // scaleControl Boolean
-    // scaleControlOptions google.maps.ScaleControlOptions
-    // scrollwheel Boolean
-    // signInControl Boolean
-    // streetView google.maps.StreetViewPanorama
-    // streetViewControl Boolean
-    // streetViewControlOptions google.maps.StreetViewControlOptions
-    // styles [google.maps.MapTypeStyle]
-    // zoomControl Boolean
-    // zoomControlOption google.maps.ZoomControlOptions
+  const proxy = GoogleMapProxy.create({
+    content: new google.maps.Map(element, MAP_DEFAULTS) // instantiate Google Map
   });
 
-  return proxy.create({ content: map });
+  // Set map defaults via proxy API
+  Object.keys(settings).forEach((key) =>
+    proxy.set(key, settings[key]));
+
+  return proxy;
 }
--- tests/unit/google-map-test.js
+++ tests/unit/google-map-test.js
@@ -18,15 +18,16 @@ test('it set map default options', function(assert) {
 });
 
 test('it returns the center of the Map instance', function(assert) {
+  const expected = { lat: 1, lng: 1 };
   const map = googleMap(document.createElement('div'));
-  map.content.setCenter({ lat: 1, lng: 1 });
+  map.content.setCenter(expected);
 
   const center = map.get('center');
-  assert.equal(center.lat, 1, 'resolves correct latitude');
-  assert.equal(center.lng, 1, 'resolves correct longitude');
+  assert.equal(center.lat, expected.lat, 'resolves correct latitude');
+  assert.equal(center.lng, expected.lng, 'resolves correct longitude');
 });
 
-test('it returns the center of the Map instance', function(assert) {
+test('it allows valid updates of the map center', function(assert) {
   const expected = {lat: 2, lng: 2};
   const map = googleMap(document.createElement('div'));
   map.content.setCenter({ lat: 1, lng: 1 });
@@ -37,37 +38,41 @@ test('it returns the center of the Map instance', function(assert) {
   assert.equal(map.get('center.lng'), expected.lat, 'updated longitude of map');
 });
 
+test('it returns the minZoom', function(assert) {
+  const expected = 2;
+  const map = googleMap(document.createElement('div'), {minZoom: expected});
+  assert.equal(map.get('minZoom'), expected, 'resolves default min zoom');
+});
+
 test('it only allows setting a valid min zoom', function(assert) {
   const map = googleMap(document.createElement('div'), {maxZoom: 5});
   assert.throws(() => map.set('minZoom', 5), 'does not allow min zoom above max zoom');
-  assert.equal(map.set('minZoom', 4), map.content.minZoom, 'updated min zoom of map')
+  assert.equal(map.set('minZoom', 4), map.content.minZoom, 'updated min zoom of map');
+});
+
+test('it returns the maxZoom', function(assert) {
+  const expected = 5;
+  const map = googleMap(document.createElement('div'), {maxZoom: expected});
+  assert.equal(map.get('maxZoom'), expected, 'resolves default max zoom');
 });
 
 test('it only allows setting a valid max zoom', function(assert) {
   const map = googleMap(document.createElement('div'), {minZoom: 5});
   assert.throws(() => map.set('maxZoom', 5), 'does not allow max zoom below min zoom');
-  assert.equal(map.set('maxZoom', 6), map.content.maxZoom, 'updated max zoom of map')
+  assert.equal(map.set('maxZoom', 6), map.content.maxZoom, 'updated max zoom of map');
 });
 
 test('it returns the zoom level', function(assert) {
-  const expected = 4;
-  const map = googleMap(document.createElement('div'));
-
-  map.content.setZoom(expected);
-
+  const expected = 5;
+  const map = googleMap(document.createElement('div'), {zoom: expected});
   assert.equal(map.get('zoom'), expected, 'resolves correct zoom level');
 });
 
-test('it throws if setting invalid zoom level', function(assert) {
+test('it allows setting a valid zoom level', function(assert) {
+  const expected = 4;
   const map = googleMap(document.createElement('div'), {minZoom: 1, maxZoom: 10});
   assert.throws(() => map.set('zoom', 0), 'does not set below minimum');
   assert.throws(() => map.set('zoom', 11), 'does not set above maximum');
-});
-
-test('it updates map instance zoom with valid zoom level', function(assert) {
-  const expected = 4;
-  const map = googleMap(document.createElement('div'));
-  map.content.setZoom(1);
 
   map.set('zoom', expected);
 
@@ -75,8 +80,9 @@ test('it updates map instance zoom with valid zoom level', function(assert) {
 });
 
 test('it returns the map type id', function(assert) {
-  const map = googleMap(document.createElement('div'));
-  assert.equal(map.get('mapTypeId'), map.content.getMapTypeId().toUpperCase(), 'resolves correct map type');
+  const expected = 'HYBRID';
+  const map = googleMap(document.createElement('div'), {mapTypeId: expected});
+  assert.equal(map.get('mapTypeId'), expected, 'resolves configured map type');
 });
 
 test('it allows setting a valid map type id', function(assert) {
@@ -86,24 +92,80 @@ test('it allows setting a valid map type id', function(assert) {
   assert.throws(() => map.set('mapTypeId', 'invalid'), 'only accpets valid map type ids');
 
   map.set('mapTypeId', expected.toLowerCase());
-  assert.equal(expected, map.content.getMapTypeId().toUpperCase(), 'resolves new map type id');
+  assert.equal(map.content.getMapTypeId().toUpperCase(), expected, 'resolves new map type id');
+});
+
+test('it returns the map clickableIcons', function(assert) {
+  const expected = false;
+  const map = googleMap(document.createElement('div'), {clickableIcons: expected});
+  assert.equal(map.get('clickableIcons'), expected, 'resolves configured clickableIcons');
 });
 
 test('it allows setting a valid clickable icons', function(assert) {
   const expected = false;
-  const map = googleMap(document.createElement('div'), { clickableIcons: true });
+  const map = googleMap(document.createElement('div'));
 
   assert.throws(() => map.set('clickableIcons', 'non-boolean'), 'only accpets boolean value');
 
   map.set('clickableIcons', expected);
-  assert.equal(expected, map.content.getClickableIcons(), 'resolves new clickable icons');
+  assert.equal(map.content.getClickableIcons(), expected, 'resolves new clickable icons');
 });
 
-test('it only allows setting a valid tilt perspective', function(assert) {
-  const expected = 45;
-  const map = googleMap(document.createElement('div'), {tilt: 0});
+test('it returns the map tilt', function(assert) {
+  const expected = 0; // NOTE 45 creates an unreliable test condition
+  const map = googleMap(document.createElement('div'), {tilt: expected});
+  assert.equal(map.get('tilt'), expected, 'resolves configured tilt');
+});
+
+test('it only calls `setTilt` with a valid tilt perspective', function(assert) {
+  const map = googleMap(document.createElement('div'));
   assert.throws(() => map.set('tilt', 32), 'does not allow invalid tilt values');
 
-  map.set('tilt', expected);
-  assert.equal(expected, map.content.getTilt(), 'updated tilt of map')
+  let wasCalled = false;
+  map.content.setTilt = () => wasCalled = true;
+
+  map.set('tilt', 45);
+  assert.equal(wasCalled, true, 'updated tilt of map');
+});
+
+test('it returns the map heading', function(assert) {
+  const expected = 0;
+  const map = googleMap(document.createElement('div'), {heading: expected});
+  assert.equal(map.get('heading'), expected, 'resolves configured heading');
+});
+
+test('it only allows setting a valid heading', function(assert) {
+  const expected = 0;
+  const map = googleMap(document.createElement('div'), {heading: 1});
+
+  assert.throws(() => map.set('heading', 'non-number'), 'only accpets numeric value');
+
+  map.set('heading', expected);
+  assert.equal(map.content.getHeading(), expected, 'resolves new heading');
+});
+
+test('it returns the static map properties', function(assert) {
+  const expected = {
+    disableDefaultUI: true,
+    disableDoubleClickZoom: true,
+    draggable: false,
+    draggableCursor: 'pointer',
+    fullscreenControl: false
+  };
+  const map = googleMap(document.createElement('div'), expected);
+  assert.equal(map.get('disableDefaultUI'), expected.disableDefaultUI, 'resolves configured disableDefaultUI');
+  assert.equal(map.get('disableDoubleClickZoom'), expected.disableDoubleClickZoom, 'resolves configured disableDoubleClickZoom');
+  assert.equal(map.get('draggable'), expected.draggable, 'resolves configured draggable');
+  assert.equal(map.get('draggableCursor'), expected.draggableCursor, 'resolves configured draggableCursor');
+  assert.equal(map.get('fullscreenControl'), expected.fullscreenControl, 'resolves configured fullscreenControl');
+});
+
+test('it only allows setting a valid disableDefaultUI', function(assert) {
+  const expected = true;
+  const map = googleMap(document.createElement('div'));
+
+  assert.throws(() => map.set('disableDefaultUI', 'non-boolean'), 'only accpets boolean value');
+
+  map.set('disableDefaultUI', expected);
+  assert.equal(map.get('disableDefaultUI'), expected, 'resolves new disableDefaultUI');
 });
