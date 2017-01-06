200d8491cbd8de31557a65f2cbbdab186b699fa2
Matthew Jensen <matt@jwc.works>
2017-01-06 16:45:14 -0600

    Add google maps circle factory

---
+++ addon/components/g-map-circle/factory.js
@@ -0,0 +1,337 @@
+import Ember from 'ember';
+import {assert} from 'ember-metal/utils';
+import {assign} from 'ember-platform';
+import computed from 'ember-computed';
+
+const CIRCLE_DEFAULTS = {
+  clickable: true,
+  draggable: false,
+  editable: false,
+  fillOpacity: 1,
+  strokeOpacity: 1,
+  strokePosition: 'CENTER'
+};
+
+export const GoogleMapCircleProxy = Ember.ObjectProxy.extend({
+  /**
+   * @required
+   * @type {Object}
+   * Circle center
+   */
+  center: computed({
+    get() {
+      const latLng = this.content.getCenter();
+      return {lat: latLng.lat(), lng: latLng.lng()};
+    },
+
+    set(key, value) {
+      assert('g-map-circle `center` is an Object', typeof value === 'object');
+
+      const {lat, lng} = value;
+
+      assert('g-map-circle `center.lat` is a Number', typeof lat === 'number' && lat === lat);
+      assert('g-map-circle `center.lng` is a Number', typeof lng === 'number' && lng === lng);
+
+      this.content.setCenter(value);
+      return this.get('center');
+    }
+  }).volatile(),
+
+  /**
+  * @type {Boolean}
+  * Indicates whether this Circle handles mouse events.
+  */
+  clickable: computed({
+    get() {
+      return this.content.clickable;
+    },
+
+    set(key, value) {
+      if (!value) { value = false; } // remove
+      assert('g-map-circle `clickableIcons` is a Boolean', typeof value === 'boolean');
+
+      // TODO is clickable while dragging warning
+
+      this.content.clickable = value;
+      return value;
+    }
+  }),
+
+  /**
+   * @type {Boolean}
+   * If set to true, the user can drag this circle over the map.
+   */
+  draggable: computed({
+    get() {
+      return this.content.getDraggable();
+    },
+
+    set(key, value) {
+      if (!value) { value = false; } // remove
+
+      assert('g-map-circle `draggable` is a Boolean', typeof value === 'boolean');
+
+      this.content.setDraggable(value);
+      return value;
+    }
+  }),
+
+  /**
+   * @type {Boolean}
+   * If set to true, the user can edit this circle by dragging the
+   * control points shown at the center and around the circumference
+   * of the circle.
+   */
+  editable: computed({
+    get() {
+      return this.content.getEditable();
+    },
+
+    set(key, value) {
+      if (!value) { value = false; } // remove
+
+      assert('g-map-circle `editable` is a Boolean', typeof value === 'boolean');
+
+      this.content.setEditable(value);
+      return value;
+    }
+  }),
+
+  /**
+   * @type {String}
+   * The fill color. All CSS3 colors are supported except for extended
+   * named colors.
+   */
+  fillColor: computed({
+    get() {
+      return this.content.fillColor;
+    },
+
+    set(key, value) {
+      if (!value) { value = ''; } // remove
+
+      assert('g-map-circle `fillColor` is a String', typeof value === 'string');
+
+      this.content.fillColor = value;
+      return value;
+    }
+  }),
+
+  /**
+   * @type {Number}
+   * The fill opacity between 0.0 and 1.0
+   */
+  fillOpacity: computed({
+    get() {
+      return this.content.fillOpacity;
+    },
+
+    set(key, value) {
+      if (!value && value !== 0) {
+        value = CIRCLE_DEFAULTS.fillOpacity; // reset
+      }
+
+      assert('g-map-circle `fillOpacity` is a Number', typeof value === 'number');
+      assert('g-map-circle `fillOpacity` is not greater than 1', value <= 1);
+      assert('g-map-circle `fillOpacity` is not less than 0', value >= 0);
+
+      this.content.fillOpacity = value;
+      return value;
+    }
+  }),
+
+  /**
+   * @required
+   * @type {Number}
+   * The radius in meters on the Earth's surface
+   */
+  radius: computed({
+    get() {
+      return this.content.getRadius();
+    },
+
+    set(key, value) {
+      if (!value) { value = 0; } // remove
+
+      assert('g-map-circle `radius` is a Number', typeof value === 'number' && value === value);
+
+      this.content.setRadius(value);
+      return value;
+    }
+  }).volatile(),
+
+  /**
+   * @type {String}
+   * The stroke color. All CSS3 colors are supported except for extended
+   * named colors.
+   */
+  strokeColor: computed({
+    get() {
+      return this.content.strokeColor;
+    },
+
+    set(key, value) {
+      if (!value) { value = ''; } // remove
+
+      assert('g-map-circle `strokeColor` is a String', typeof value === 'string');
+
+      this.content.strokeColor = value;
+      return value;
+    }
+  }),
+
+  /**
+   * @type {Number}
+   * The stroke opacity between 0.0 and 1.0
+   */
+  strokeOpacity: computed({
+    get() {
+      return this.content.strokeOpacity;
+    },
+
+    set(key, value) {
+      if (!value && value !== 0) {
+        value = CIRCLE_DEFAULTS.strokeOpacity; // reset
+      }
+
+      assert('g-map-circle `strokeOpacity` is a Number', typeof value === 'number');
+      assert('g-map-circle `strokeOpacity` is not greater than 1', value <= 1);
+      assert('g-map-circle `strokeOpacity` is not less than 0', value >= 0);
+
+      this.content.strokeOpacity = value;
+      return value;
+    }
+  }),
+
+  /**
+   * @type {String}
+   * The stroke position.
+   */
+  strokePosition: computed({
+    get() {
+      return getStrokePosition(this.content.strokePosition);
+    },
+
+    set(key, value) {
+      if (!value) { value = CIRCLE_DEFAULTS.strokePosition; } // reset
+
+      assert('g-map-circle `strokePosition` is a String', typeof value === 'string');
+
+      const id = getStrokePositionId(value);
+
+      assert('g-map-circle `strokePosition` is a valid stroke position', typeof id === 'number');
+
+      this.content.strokePosition = id;
+      return value;
+    }
+  }),
+
+  /**
+   * @type {Number}
+   * The stroke width in pixels.
+   */
+  strokeWeight: computed({
+    get() {
+      return this.content.strokeWeight;
+    },
+
+    set(key, value) {
+      if (!value) { value = 0; } // remove
+
+      assert('g-map-circle `strokeWeight` is a Number', typeof value === 'number');
+
+      this.content.strokeWeight = value;
+      return value;
+    }
+  }),
+
+  /**
+   * @type {Boolean}
+   * @default {true}
+   * Whether this circle is visible on the map.
+   */
+  visible: computed({
+    get() {
+      return this.content.getVisible();
+    },
+
+    set(key, value) {
+      if (!value) { value = false; } // remove
+
+      assert('g-map-circle `visible` is a Boolean', typeof value === 'boolean');
+
+      this.content.setVisible(value);
+      return value;
+    }
+  }),
+
+  /**
+  * @type {Number|Undefined}
+  * The zIndex compared to other polys.
+  */
+  zIndex: computed({
+    get() {
+      return this.content.zIndex;
+    },
+
+    set(key, value) {
+      if (!value && value !== 0) {
+        this.content.zIndex = null; // remove
+        return;
+      }
+
+      assert('g-map-circle `zIndex` is a Number', typeof value === 'number');
+      assert('g-map-circle `zIndex` is a Whole Number', value % 1 === 0);
+
+      this.content.zIndex = value;
+      return value;
+    }
+  })
+});
+
+/**
+ * @param  {Map}    canvas   Google Maps' Map
+ * @param  {Object} options  Circle instance defaults (requires center, radius)
+ * @return {ObjectProxy}  Ember.ObjectProxy instance
+ * Render a new Google Map Circle on a canvas with the given options
+ * and return its' Google Map Center Proxy instance
+ */
+export default function googleMapCircle(map, options = {}) {
+  assert('Google Map Circle requires a Google Map instance', map instanceof google.maps.Map);
+  assert('Google Map Circle requires a center', options.center);
+  assert('Google Map Circle requires a radius', options.radius);
+
+  const proxy = GoogleMapCircleProxy.create({
+    content: new google.maps.Circle({map})
+  });
+
+  const settings = assign({}, CIRCLE_DEFAULTS);
+  assign(settings, options);
+
+  // Set defaults via proxy API
+  Object.keys(settings).forEach((key) =>
+    proxy.set(key, settings[key]));
+
+  return proxy;
+}
+
+/**
+ * @param  {String} path   Stroke position
+ * @return {Number}        Stroke position id
+ * Get the id of a preconfigured stroke position by name
+ */
+export function getStrokePositionId(position) {
+  position = `${position}`.toUpperCase();
+  return google.maps.StrokePosition[position];
+}
+
+/**
+ * @param  {Number} id Stroke position id
+ * @return {String}    Stroke position
+ * Get a name of a preconfigured stroke position from its' id value
+ */
+export function getStrokePosition(id) {
+  id = parseInt(id, 10);
+  return Object.keys(google.maps.StrokePosition).filter((position) =>
+    google.maps.StrokePosition[position] === id)[0];
+}
---
+++ tests/unit/components/g-map-circle/factory-test.js
@@ -0,0 +1,320 @@
+import googleMapCircle from 'ember-cli-g-maps/components/g-map-circle/factory';
+import {module, test} from 'qunit';
+import {assign} from 'ember-platform';
+import {createGoogleMap} from '../../../helpers/google-maps';
+
+module('Unit | Factory | Google Map Circle');
+
+const DEFAULTS  = {
+  center: {lat: 1, lng: 1},
+  radius: 10000
+};
+
+test('it returns a Google Map Circle instance as content', function(assert) {
+  const circle = googleMapCircle(createGoogleMap(), assign({}, DEFAULTS));
+  assert.ok(circle.content instanceof google.maps.Circle);
+});
+
+test('it returns the configured center of the circle instance', function(assert) {
+  const expected = DEFAULTS.center;
+  const circle = googleMapCircle(createGoogleMap(), assign({}, DEFAULTS));
+  assert.deepEqual(circle.get('center'), expected, 'resolves correct center');
+});
+
+test('it allows valid updates of the circle center', function(assert) {
+  const expected = {lat: 2, lng: 2};
+  const circle = googleMapCircle(createGoogleMap(), assign({}, DEFAULTS));
+
+  assert.throws(() => circle.set('center', [-34, -90]), 'only accepts latLng literal');
+  assert.throws(() => circle.set('center', {lat: 34}), 'requires a `lng` property');
+  assert.throws(() => circle.set('center', {lng: -90}), 'requires a `lat` property');
+
+  circle.set('center', expected);
+  assert.deepEqual(circle.get('center'), expected, 'updated circle center');
+});
+
+test('it returns the configured clickable setting', function(assert) {
+  const expected = {clickable: false};
+  const circle = googleMapCircle(createGoogleMap(), assign(expected, DEFAULTS));
+  assert.equal(circle.get('clickable'), expected.clickable, 'resolves configured clickable');
+});
+
+test('it only allows setting a valid clickable value', function(assert) {
+  const circle = googleMapCircle(createGoogleMap(), assign({}, DEFAULTS));
+
+  assert.throws(() => circle.set('clickable', 'non-boolean'), 'rejects non-boolean value');
+
+  circle.set('clickable', false);
+  circle.notifyPropertyChange('clickable');
+  assert.equal(circle.get('clickable'), false, 'updated clickable');
+});
+
+test('it allows setting clickable to false with any falsey value', function(assert) {
+  const circle = googleMapCircle(createGoogleMap(), assign({}, DEFAULTS));
+  circle.set('clickable', NaN);
+  circle.notifyPropertyChange('clickable');
+  assert.equal(circle.get('clickable'), false, 'updated clickable with `NaN`');
+});
+
+test('it returns the configured draggable setting', function(assert) {
+  const expected = {draggable: true};
+  const circle = googleMapCircle(createGoogleMap(), assign(expected, DEFAULTS));
+  assert.equal(circle.get('draggable'), expected.draggable, 'resolves configured draggable');
+});
+
+test('it only allows setting a valid draggable value', function(assert) {
+  const circle = googleMapCircle(createGoogleMap(), assign({}, DEFAULTS));
+
+  assert.throws(() => circle.set('draggable', 'non-boolean'), 'rejects non-boolean value');
+
+  circle.set('draggable', true);
+  circle.notifyPropertyChange('draggable');
+  assert.equal(circle.get('draggable'), true, 'updated draggable');
+});
+
+test('it allows setting draggable to false with any falsey value', function(assert) {
+  const circle = googleMapCircle(createGoogleMap(), assign({draggable: true}, DEFAULTS));
+  circle.set('draggable', null);
+  circle.notifyPropertyChange('draggable');
+  assert.equal(circle.get('draggable'), false, 'updated draggable with `null`');
+});
+
+test('it returns the configured editable setting', function(assert) {
+  const expected = {editable: true};
+  const circle = googleMapCircle(createGoogleMap(), assign(expected, DEFAULTS));
+  assert.equal(circle.get('editable'), expected.editable, 'resolves configured editable');
+});
+
+test('it only allows setting a valid editable value', function(assert) {
+  const circle = googleMapCircle(createGoogleMap(), assign({}, DEFAULTS));
+
+  assert.throws(() => circle.set('editable', 'non-boolean'), 'rejects non-boolean value');
+
+  circle.set('editable', true);
+  circle.notifyPropertyChange('editable');
+  assert.equal(circle.get('editable'), true, 'updated editable');
+});
+
+test('it allows setting editable to false with any falsey value', function(assert) {
+  const circle = googleMapCircle(createGoogleMap(), assign({editable: true}, DEFAULTS));
+  circle.set('editable', NaN);
+  circle.notifyPropertyChange('editable');
+  assert.equal(circle.get('editable'), false, 'updated editable with `NaN`');
+});
+
+test('it returns the configured fillColor setting', function(assert) {
+  const expected = {fillColor: '#000000'};
+  const circle = googleMapCircle(createGoogleMap(), assign(expected, DEFAULTS));
+  assert.equal(circle.get('fillColor'), expected.fillColor, 'resolves configured fillColor');
+});
+
+test('it only allows setting a valid fillColor value', function(assert) {
+  const expected = '#000000';
+  const circle = googleMapCircle(createGoogleMap(), assign({}, DEFAULTS));
+
+  assert.throws(() => circle.set('fillColor', true), 'rejects non-string value');
+
+  circle.set('fillColor', expected);
+  circle.notifyPropertyChange('fillColor');
+  assert.equal(circle.get('fillColor'), expected, 'updated fillColor');
+});
+
+test('it allows setting fillColor to none with any falsey value', function(assert) {
+  const circle = googleMapCircle(createGoogleMap(), assign({fillColor: '#000000'}, DEFAULTS));
+  circle.set('fillColor', null);
+  circle.notifyPropertyChange('fillColor');
+  assert.equal(circle.get('fillColor'), '', 'updated fillColor with `null`');
+});
+
+test('it returns the configured fillOpacity setting', function(assert) {
+  const expected = {fillOpacity: 0.85};
+  const circle = googleMapCircle(createGoogleMap(), assign(expected, DEFAULTS));
+  assert.equal(circle.get('fillOpacity'), expected.fillOpacity, 'resolves configured fillOpacity');
+});
+
+test('it only allows setting a valid fillOpacity value', function(assert) {
+  const expected = 0.85;
+  const circle = googleMapCircle(createGoogleMap(), assign({}, DEFAULTS));
+
+  assert.throws(() => circle.set('fillOpacity', true), 'rejects non-numeric value');
+  assert.throws(() => circle.set('fillOpacity', 1.1), 'rejects opacity greater than 1');
+  assert.throws(() => circle.set('fillOpacity', -0.1), 'rejects opacity less than 1');
+
+  circle.set('fillOpacity', expected);
+  circle.notifyPropertyChange('fillOpacity');
+  assert.equal(circle.get('fillOpacity'), expected, 'updated fillOpacity');
+});
+
+test('it allows reseting fillOpacity to default with any falsey value', function(assert) {
+  const circle = googleMapCircle(createGoogleMap(), assign({}, DEFAULTS));
+  const expected = circle.get('fillOpacity');
+  circle.set('fillOpacity', 0.85);
+  circle.set('fillOpacity', null);
+  circle.notifyPropertyChange('fillOpacity');
+  assert.equal(circle.get('fillOpacity'), expected, 'reset fillOpacity with `null`');
+});
+
+test('it returns the configured radius setting', function(assert) {
+  const expected = {radius: 1000};
+  const circle = googleMapCircle(createGoogleMap(), assign(expected, DEFAULTS));
+  assert.equal(circle.get('radius'), expected.radius, 'resolves configured radius');
+});
+
+test('it only allows setting a valid radius value', function(assert) {
+  const expected = 1000;
+  const circle = googleMapCircle(createGoogleMap(), assign({}, DEFAULTS));
+
+  assert.throws(() => circle.set('radius', true), 'rejects non-numeric value');
+
+  circle.set('radius', expected);
+  assert.equal(circle.get('radius'), expected, 'updated radius');
+});
+
+test('it returns the configured strokeColor setting', function(assert) {
+  const expected = {strokeColor: '#000000'};
+  const circle = googleMapCircle(createGoogleMap(), assign(expected, DEFAULTS));
+  assert.equal(circle.get('strokeColor'), expected.strokeColor, 'resolves configured strokeColor');
+});
+
+test('it only allows setting a valid strokeColor value', function(assert) {
+  const expected = '#000000';
+  const circle = googleMapCircle(createGoogleMap(), assign({}, DEFAULTS));
+
+  assert.throws(() => circle.set('strokeColor', true), 'rejects non-string value');
+
+  circle.set('strokeColor', expected);
+  circle.notifyPropertyChange('strokeColor');
+  assert.equal(circle.get('strokeColor'), expected, 'updated strokeColor');
+});
+
+test('it allows setting strokeColor to none with any falsey value', function(assert) {
+  const circle = googleMapCircle(createGoogleMap(), assign({strokeColor: '#000000'}, DEFAULTS));
+  circle.set('strokeColor', null);
+  circle.notifyPropertyChange('strokeColor');
+  assert.equal(circle.get('strokeColor'), '', 'updated strokeColor with `null`');
+});
+
+test('it returns the configured strokeOpacity setting', function(assert) {
+  const expected = {strokeOpacity: 0.85};
+  const circle = googleMapCircle(createGoogleMap(), assign(expected, DEFAULTS));
+  assert.equal(circle.get('strokeOpacity'), expected.strokeOpacity, 'resolves configured strokeOpacity');
+});
+
+test('it only allows setting a valid strokeOpacity value', function(assert) {
+  const expected = 0.85;
+  const circle = googleMapCircle(createGoogleMap(), assign({}, DEFAULTS));
+
+  assert.throws(() => circle.set('strokeOpacity', true), 'rejects non-numeric value');
+  assert.throws(() => circle.set('strokeOpacity', 1.1), 'rejects opacity greater than 1');
+  assert.throws(() => circle.set('strokeOpacity', -0.1), 'rejects opacity less than 1');
+
+  circle.set('strokeOpacity', expected);
+  circle.notifyPropertyChange('strokeOpacity');
+  assert.equal(circle.get('strokeOpacity'), expected, 'updated strokeOpacity');
+});
+
+test('it allows reseting strokeOpacity to default with any falsey value', function(assert) {
+  const circle = googleMapCircle(createGoogleMap(), assign({}, DEFAULTS));
+  const expected = circle.get('strokeOpacity');
+  circle.set('strokeOpacity', 0.85);
+  circle.set('strokeOpacity', null);
+  circle.notifyPropertyChange('strokeOpacity');
+  assert.equal(circle.get('strokeOpacity'), expected, 'reset strokeOpacity with `null`');
+});
+
+test('it returns the configured strokePosition setting', function(assert) {
+  const expected = {strokePosition: 'INSIDE'};
+  const circle = googleMapCircle(createGoogleMap(), assign(expected, DEFAULTS));
+  assert.equal(circle.get('strokePosition'), expected.strokePosition, 'resolves configured strokePosition');
+});
+
+test('it only allows setting a valid strokePosition value', function(assert) {
+  const expected = 'INSIDE';
+  const circle = googleMapCircle(createGoogleMap(), assign({}, DEFAULTS));
+
+  assert.throws(() => circle.set('strokePosition', true), 'rejects non-string value');
+  assert.throws(() => circle.set('strokePosition', 'NON-POSITION'), 'rejects an invalid stroke position');
+
+  circle.set('strokePosition', expected);
+  circle.notifyPropertyChange('strokePosition');
+  assert.equal(circle.get('strokePosition'), expected, 'updated strokePosition');
+});
+
+test('it allows reseting strokePosition to default with any falsey value', function(assert) {
+  const circle = googleMapCircle(createGoogleMap(), assign({}, DEFAULTS));
+  const expected = circle.get('strokePosition');
+  circle.set('strokePosition', 'INSIDE');
+  circle.set('strokePosition', null);
+  circle.notifyPropertyChange('strokePosition');
+  assert.equal(circle.get('strokePosition'), expected, 'reset strokePosition with `null`');
+});
+
+test('it returns the configured strokeWeight setting', function(assert) {
+  const expected = {strokeWeight: 20};
+  const circle = googleMapCircle(createGoogleMap(), assign(expected, DEFAULTS));
+  assert.equal(circle.get('strokeWeight'), expected.strokeWeight, 'resolves configured strokeWeight');
+});
+
+test('it only allows setting a valid strokeWeight value', function(assert) {
+  const expected = 20;
+  const circle = googleMapCircle(createGoogleMap(), assign({}, DEFAULTS));
+
+  assert.throws(() => circle.set('strokeWeight', true), 'rejects non-numeric value');
+
+  circle.set('strokeWeight', expected);
+  assert.equal(circle.get('strokeWeight'), expected, 'updated strokeWeight');
+});
+
+test('it allows setting strokeWeight to 0 with any falsey value', function(assert) {
+  const circle = googleMapCircle(createGoogleMap(), assign({strokeWeight: 30}, DEFAULTS));
+  circle.set('strokeWeight', NaN);
+  circle.notifyPropertyChange('strokeWeight');
+  assert.equal(circle.get('strokeWeight'), 0, 'updated strokeWeight with `NaN`');
+});
+
+test('it returns the configured visible setting', function(assert) {
+  const expected = {visible: false};
+  const circle = googleMapCircle(createGoogleMap(), assign(expected, DEFAULTS));
+  assert.equal(circle.get('visible'), expected.visible, 'resolves configured visible');
+});
+
+test('it only allows setting a valid visible value', function(assert) {
+  const circle = googleMapCircle(createGoogleMap(), assign({}, DEFAULTS));
+
+  assert.throws(() => circle.set('visible', 'non-boolean'), 'rejects non-boolean value');
+
+  circle.set('visible', false);
+  circle.notifyPropertyChange('visible');
+  assert.equal(circle.get('visible'), false, 'updated visible');
+});
+
+test('it allows setting visible to false with any falsey value', function(assert) {
+  const circle = googleMapCircle(createGoogleMap(), assign({}, DEFAULTS));
+  circle.set('visible', NaN);
+  circle.notifyPropertyChange('visible');
+  assert.equal(circle.get('visible'), false, 'updated visible with `NaN`');
+});
+
+test('it returns the configured zIndex setting', function(assert) {
+  const expected = {zIndex: 100};
+  const circle = googleMapCircle(createGoogleMap(), assign(expected, DEFAULTS));
+  assert.equal(circle.get('zIndex'), expected.zIndex, 'resolves configured zIndex');
+});
+
+test('it only allows setting a valid zIndex value', function(assert) {
+  const expected = 100;
+  const circle = googleMapCircle(createGoogleMap(), assign({}, DEFAULTS));
+
+  assert.throws(() => circle.set('zIndex', true), 'rejects non-numeric value');
+  assert.throws(() => circle.set('zIndex', 100.1), 'rejects floating point numbers');
+
+  circle.set('zIndex', expected);
+  assert.equal(circle.get('zIndex'), expected, 'updated zIndex');
+});
+
+test('it allows setting zIndex to false with any falsey value', function(assert) {
+  const circle = googleMapCircle(createGoogleMap(), assign({zIndex: 9}, DEFAULTS));
+  circle.set('zIndex', NaN);
+  circle.notifyPropertyChange('zIndex');
+  assert.equal(circle.get('zIndex'), undefined, 'updated zIndex with `NaN`');
+});
